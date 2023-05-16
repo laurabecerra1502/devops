6bad5e2a958792896feb566f68eec3a559261678
laurabecerra1502 <laurabecerra@periferia-it.com>
2023-05-16 11:55:55 -0500

    d

--- vars/reto.groovy
+++ vars/reto.groovy
@@ -8,7 +8,7 @@ def call(Map params){
                 steps {
                     script {
                         def z = new com.devops.build()
-                        z.build()
+                        z.build(msn)
                     }
                 }
                 
