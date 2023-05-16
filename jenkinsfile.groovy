6aad278ae6e59c689f38c486f39260b8e0892ffe
laurabecerra1502 <laurabecerra@periferia-it.com>
2023-05-16 10:59:59 -0500

    d

---
+++ jenkinsfile.groovy
@@ -0,0 +1,33 @@
+def jenkinsfile(Map params){
+
+    pipeline {
+        agent any
+
+        stages {
+            stage('Hello') {
+                steps {
+                    script {
+                        build.good()
+                    }
+                }
+                
+            }
+        
+            stage('Checkout') {
+                steps {
+                    script {
+                        sonarqube.checkout()
+                    }
+                }
+            }
+
+            stage('scan') {
+                steps {
+                    script {
+                        sonarqube.scan()
+                    }  
+                }        
+            }
+        }
+    }
+}
