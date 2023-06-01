aa3325adb8d11e468ab55580453e41656e5c9ddc
laurabecerra1502 <laurabecerra@periferia-it.com>
2023-06-01 15:49:55 -0500

    dd

--- vars/libdevops.groovy
+++ vars/libdevops.groovy
@@ -86,12 +86,9 @@ def call(Map params){
             stage('Escaneo de la aplicación') {
                 steps {
                     script{
-                        /*sh 'docker network create testapp'
                         sh 'docker run -dt --name owasp -v owasp_data:/zap/reports --user root -t owasp/zap2docker-stable /bin/bash'
                         sh 'docker exec owasp mkdir /zap/wrk'
-                        sh 'docker network connect testapp 47bf1789dd52'
-                        sh 'docker network connect testapp 2a4896e12971'*/
-                        sh 'docker exec owasp zap-full-scan.py -t http://localhost:8040/ -r report.html -I'
+                        sh 'docker exec owasp zap-full-scan.py -t http://app:8040/ -r report.html -I'
                         sh 'docker cp owasp:/zap/wrk/report.html report.html'
                         sh 'docker cp report.html jenkins:/var/jenkins_home/workspace/devops_reto/'
 
