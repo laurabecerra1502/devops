95ef9c3cd7d56f8750a00c9b63c0cea4ce192345
laurabecerra1502 <laurabecerra@periferia-it.com>
2023-06-01 09:07:40 -0500

    deploy

--- src/org/devops/deploy.groovy
+++ src/org/devops/deploy.groovy
@@ -1,5 +1,5 @@
 package org.devops
 
 def deployimage(PROJECT){
-    sh "docker run --name ${PROJECT} -p 80${env.BUILD_ID}:3000 -d ${PROJECT}"
+    sh "docker run --name ${PROJECT} -p 80${env.BUILD_ID}:3000 -d laurabecerra/${PROJECT}:${env.BUILD_ID}"
 }
