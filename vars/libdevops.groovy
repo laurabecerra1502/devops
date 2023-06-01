d68507d284589cefd7bb6edc297814751617b2ca
laurabecerra1502 <laurabecerra@periferia-it.com>
2023-06-01 16:31:27 -0500

    dd

--- vars/libdevops.groovy
+++ vars/libdevops.groovy
@@ -88,9 +88,9 @@ def call(Map params){
                     script{
                         /*sh 'docker run -dt --name owasp -v owasp_data:/zap/reports --user root -t owasp/zap2docker-stable /bin/bash'
                         sh 'docker exec owasp mkdir /zap/wrk'*/
-                        sh 'docker exec owasp zap-full-scan.py -t http://app:8040/ -r report.html -I'
-                        sh 'docker cp owasp:/zap/wrk/report.html report.html'
-                        sh 'docker cp report.html jenkins:/var/jenkins_home/workspace/devops_reto/'
+                        sh 'docker exec owasp zap-full-scan.py -t http://app:8040/ -r reporteowasp.html -I'
+                        sh 'docker cp owasp:/zap/wrk/report.html reporteowasp.html'
+                        sh 'docker cp reporteowasp.html jenkins:/var/jenkins_home/workspace/devops_reto/'
 
                         
                     }
