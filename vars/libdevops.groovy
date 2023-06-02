dc0b1b2fc2f04e643ddfb2b272fe660d2c817d93
laurabecerra1502 <laurabecerra@periferia-it.com>
2023-06-01 23:28:27 -0500

    cambios

--- vars/libdevops.groovy
+++ vars/libdevops.groovy
@@ -7,7 +7,7 @@ def call(Map params){
             nodejs 'NodeJS'
         }
         
-        enviroment {
+        environment {
             branch_name = "${env.GIT_BRANCH}".split('/')[1]
             git_name = "${env.GIT_URL}".replaceAll('.+/(.+)\\.git', '$1')toLowerCase()
         }
