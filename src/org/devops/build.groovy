46c00fcdc787f44581b6d85c1eed1c863ef59c4b
laurabecerra1502 <laurabecerra@periferia-it.com>
2023-06-01 22:47:16 -0500

    cambios

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,7 +1,8 @@
 package org.devops
 
 def clone(Map params){
-    git branch: "${env.GIT_BRANCH}", url: "${params.scmUrl}"
+    "echo ${GIT_BRANCH_NAME}".
+    /*git branch: "${env.GIT_BRANCH}", url: "${params.scmUrl}"*/
 }
 
 def install(){
