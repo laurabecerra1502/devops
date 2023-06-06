ed6f799fad3b61fc9fba10720857acb7450377e9
laurabecerra1502 <laurabecerra@periferia-it.com>
2023-06-05 19:38:58 -0500

    ll

--- src/org/devops/deploy.groovy
+++ src/org/devops/deploy.groovy
@@ -1,5 +1,7 @@
 package org.devops
 
 def call(PROJECT){
-    sh "docker run --name ${PROJECT} -p 80${env.BUILD_ID}:3000 -d laurabecerra/${PROJECT}:${env.BUILD_ID}"
+    /*sh "docker run --name ${PROJECT} -p 80${env.BUILD_ID}:3000 -d laurabecerra/${PROJECT}:${env.BUILD_ID}"*/
+    def DOCKER_EXIST = sh(returnStdout: true, script: 'echo "$(docker ps -a -q --filter name=${PROJECT})"').trim()                    
+    echo "El contenedor es ${DOCKER_EXIST}"
 }
